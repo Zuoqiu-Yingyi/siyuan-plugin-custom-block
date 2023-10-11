1b55ab98813db2f6dcd248f83c3468b49d7657a5
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2023-10-11 18:44:50 +0800

    feat(custom-block): 移除重复的分割线 | Remove duplicate separator.

--- src/index.ts
+++ src/index.ts
@@ -148,7 +148,21 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
             const submenu: siyuan.IMenuItemOption[] = []; // 下级菜单
 
             /* 获得可使用的功能 */
-            const features = this.config.features.filter(feature => featureFilter(feature, context));
+            const features = this.config.features
+                .filter(feature => featureFilter(feature, context))
+                .filter((feature, index, array) => {
+                    switch (feature.mode) {
+                        case MenuItemMode.separator:
+                            if (index === 0 || index === array.length - 1) { // 移除首尾的分割线
+                                return false;
+                            }
+                            else { // 移除重复的分割线
+                                return array[index - 1].mode !== MenuItemMode.separator;
+                            }
+                        default:
+                            return true;
+                    }
+                });
 
             /* 生成菜单项 */
             features.forEach(feature => {
