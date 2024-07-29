9f3afcbb99917432a4729c6b3f07b71639809e1f
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-07-30 00:00:04 +0800

    style(custom-block): 将 `eslint` 配置应用于 `custom-block` 模块 | Apply `eslint` configuration to `custom-block` module

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,16 +1,16 @@
 <!--
  Copyright (C) 2023 Zuoqiu Yingyi
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as
  published by the Free Software Foundation, either version 3 of the
  License, or (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
- 
+
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
@@ -18,22 +18,22 @@
 <!-- 设置面板 -->
 
 <script lang="ts">
-    import Panels from "@workspace/components/siyuan/setting/panel/Panels.svelte";
-    import Panel from "@workspace/components/siyuan/setting/panel/Panel.svelte";
-    import Item from "@workspace/components/siyuan/setting/item/Item.svelte";
-    import Input from "@workspace/components/siyuan/setting/item/Input.svelte";
+    import Svg from "@workspace/components/siyuan/misc/Svg.svelte";
     import Group from "@workspace/components/siyuan/setting/item/Group.svelte";
-    import MiniItem from "@workspace/components/siyuan/setting/item/MiniItem.svelte";
-
+    import Input from "@workspace/components/siyuan/setting/item/Input.svelte";
     import { ItemType } from "@workspace/components/siyuan/setting/item/item";
-    import { type ITab } from "@workspace/components/siyuan/setting/tab";
+    import Item from "@workspace/components/siyuan/setting/item/Item.svelte";
+    import MiniItem from "@workspace/components/siyuan/setting/item/MiniItem.svelte";
+    import Panel from "@workspace/components/siyuan/setting/panel/Panel.svelte";
+    import Panels from "@workspace/components/siyuan/setting/panel/Panels.svelte";
+
+    import { MenuItemMode } from "../utils/enums";
+
+    import type { ITab } from "@workspace/components/siyuan/setting/tab";
 
     import type WebviewPlugin from "@/index";
-
     import type { IConfig } from "@/types/config";
     import type { I18N } from "@/utils/i18n";
-    import { MenuItemMode } from "../utils/enums";
-    import Svg from "@workspace/components/siyuan/misc/Svg.svelte";
 
     export let config: IConfig; // 传入的配置项
     export let plugin: InstanceType<typeof WebviewPlugin>; // 插件实例
@@ -55,13 +55,20 @@
         );
     }
 
+    function getMenuLabel(id: string): string {
+        // @ts-ignore
+        return i18n.menu[id].label;
+    }
+
+    /* eslint-disable no-unused-vars */
     enum PanelKey {
         general,
         feature,
     }
+    /* eslint-enable no-unused-vars */
 
-    let panels_focus_key = PanelKey.general;
-    const panels: ITab[] = [
+    const panels_focus_key = PanelKey.general;
+    const panels = [
         {
             key: PanelKey.general,
             text: i18n.settings.generalSettings.title,
@@ -74,26 +81,26 @@
             name: i18n.settings.featureSettings.title,
             icon: "#iconMenu",
         },
-    ];
+    ] as const satisfies ITab[];
 </script>
 
 <Panels
-    {panels}
     focus={panels_focus_key}
+    {panels}
     let:focus={focusPanel}
 >
     <!-- 常规设置面板 -->
     <Panel display={panels[0].key === focusPanel}>
         <!-- 重置设置 -->
         <Item
-            title={i18n.settings.generalSettings.reset.title}
             text={i18n.settings.generalSettings.reset.description}
+            title={i18n.settings.generalSettings.reset.title}
         >
             <Input
                 slot="input"
-                type={ItemType.button}
                 settingKey="Reset"
                 settingValue={i18n.settings.generalSettings.reset.text}
+                type={ItemType.button}
                 on:clicked={resetOptions}
             />
         </Item>
@@ -104,22 +111,22 @@
         {#each config.features as feature, i (i)}
             <!-- 非分割线 -->
             {#if feature.mode !== MenuItemMode.separator}
-                <Group title={i18n.menu[feature.id].label}>
+                <Group title={getMenuLabel(feature.id)}>
                     <MiniItem minWidth="12em">
                         <Svg
                             slot="icon"
-                            icon="#iconMenu"
                             className="svg"
+                            icon="#iconMenu"
                         />
                         <span slot="title">
                             {i18n.settings.featureSettings.menu.label}
                         </span>
                         <Input
                             slot="input"
-                            type={ItemType.checkbox}
                             settingKey="Checkbox"
                             settingValue={feature.enable}
-                            on:changed={e => {
+                            type={ItemType.checkbox}
+                            on:changed={(e) => {
                                 feature.enable = e.detail.value;
                                 updated();
                             }}
@@ -129,18 +136,18 @@
                         <MiniItem minWidth="12em">
                             <Svg
                                 slot="icon"
-                                icon="#iconTheme"
                                 className="svg"
+                                icon="#iconTheme"
                             />
                             <span slot="title">
                                 {i18n.settings.featureSettings.style.label}
                             </span>
                             <Input
                                 slot="input"
-                                type={ItemType.checkbox}
                                 settingKey="Checkbox"
                                 settingValue={feature.style}
-                                on:changed={e => {
+                                type={ItemType.checkbox}
+                                on:changed={(e) => {
                                     feature.style = e.detail.value;
                                     updated();
                                 }}
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,24 @@
-/**
- * Copyright (C) 2023 Zuoqiu Yingyi
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-import siyuan from "siyuan";
+// Copyright (C) 2023 Zuoqiu Yingyi
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 /* 静态资源 */
 import "./styles/index.less";
 
-// REF: https://zhuanlan.zhihu.com/p/401882229
-import menu from "./assets/symbols/icon-custom-block-menu.symbol?raw";
-import danmaku from "./assets/symbols/icon-custom-block-render-danmaku.symbol?raw";
-import width_auto from "./assets/symbols/icon-custom-block-width-auto.symbol?raw";
-import width_equal from "./assets/symbols/icon-custom-block-width-equal.symbol?raw";
-
 /* SDK */
 import { Client } from "@siyuan-community/siyuan-sdk";
+import siyuan from "siyuan";
 
 /* 工作空间资源 */
 import { FLAG_MOBILE } from "@workspace/utils/env/front-end";
@@ -39,18 +30,22 @@ import {
     type BlockMenuDetail,
 } from "@workspace/utils/siyuan/menu/block";
 
+import handlers from "@/utils/handlers";
+
+// REF: https://zhuanlan.zhihu.com/p/401882229
+import menu from "./assets/symbols/icon-custom-block-menu.symbol?raw";
+import danmaku from "./assets/symbols/icon-custom-block-render-danmaku.symbol?raw";
+import width_auto from "./assets/symbols/icon-custom-block-width-auto.symbol?raw";
+import width_equal from "./assets/symbols/icon-custom-block-width-equal.symbol?raw";
 /* 组件 */
 import Settings from "./components/Settings.svelte";
-
 /* 项目资源 */
 import { DEFAULT_CONFIG } from "./configs/default";
-import { featureFilter } from "./utils/filter";
 import { MenuItemMode } from "./utils/enums";
-import handlers from "@/utils/handlers";
+import { featureFilter } from "./utils/filter";
 
 /* 类型 */
 import type { IConfig } from "./types/config";
-
 
 export default class CustomBlockPlugin extends siyuan.Plugin {
     static readonly GLOBAL_CONFIG_NAME = "global-config";
@@ -60,12 +55,9 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
     public readonly logger: InstanceType<typeof Logger>;
     public readonly client: InstanceType<typeof Client>;
 
-    protected readonly STYLE_ELEMENT_ID: string;
     protected readonly SETTINGS_DIALOG_ID: string;
-    protected readonly SYSTEM_FONTS_DIALOG_ID: string;
-    protected readonly USABLE_FONTS_DIALOG_ID: string;
 
-    protected config: IConfig;
+    protected config!: IConfig;
 
     constructor(options: any) {
         super(options);
@@ -75,7 +67,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
         this.SETTINGS_DIALOG_ID = `plugin-${this.name}-settings-dialog`;
     }
 
-    onload(): void {
+    public override onload(): void {
         /* 注册图标 */
         this.addIcons([
             menu,
@@ -86,10 +78,10 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
 
         /* 加载数据 */
         this.loadData(CustomBlockPlugin.GLOBAL_CONFIG_NAME)
-            .then(config => {
+            .then((config) => {
                 this.config = mergeIgnoreArray(DEFAULT_CONFIG, config || {}) as IConfig;
             })
-            .catch(error => this.logger.error(error))
+            .catch((error) => this.logger.error(error))
             .finally(() => {
                 /* 开始监听块菜单事件 */
                 this.eventBus.on("click-blockicon", this.blockMenuEventListener);
@@ -99,7 +91,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
             });
     }
 
-    onLayoutReady(): void {
+    public override onLayoutReady(): void {
         if (compare(this.config.version, DEFAULT_CONFIG.version) < 0) {
             /* 需要重置设置选项 */
             siyuan.confirm(
@@ -113,7 +105,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
         }
     }
 
-    onunload(): void {
+    public override onunload(): void {
         /* 停止监听块菜单事件 */
         this.eventBus.off("click-blockicon", this.blockMenuEventListener);
         this.eventBus.off("click-editortitleicon", this.blockMenuEventListener);
@@ -121,21 +113,26 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
         this.removeRootAttr();
     }
 
-    openSetting(): void {
-        const that = this;
+    public override openSetting(): void {
+        // eslint-disable-next-line ts/no-this-alias
+        const plugin = this;
         const dialog = new siyuan.Dialog({
             title: `${this.i18n.displayName} <code class="fn__code">${this.name}</code>`,
-            content: `<div id="${that.SETTINGS_DIALOG_ID}" class="fn__flex-column" />`,
+            content: `<div id="${plugin.SETTINGS_DIALOG_ID}" class="fn__flex-column" />`,
             width: FLAG_MOBILE ? "92vw" : "720px",
             height: FLAG_MOBILE ? undefined : "640px",
         });
-        const settings = new Settings({
-            target: dialog.element.querySelector(`#${that.SETTINGS_DIALOG_ID}`),
-            props: {
-                config: this.config,
-                plugin: this,
-            },
-        });
+        const target = dialog.element.querySelector(`#${plugin.SETTINGS_DIALOG_ID}`);
+        if (target) {
+            const settings = new Settings({
+                target,
+                props: {
+                    config: this.config,
+                    plugin: this,
+                },
+            });
+            void settings;
+        }
     }
 
     /* 添加块菜单项 */
@@ -149,7 +146,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
 
             /* 获得可使用的功能 */
             const features = this.config.features
-                .filter(feature => featureFilter(feature, context))
+                .filter((feature) => featureFilter(feature, context))
                 .filter((feature, index, array) => {
                     switch (feature.mode) {
                         case MenuItemMode.separator:
@@ -157,7 +154,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
                                 return false;
                             }
                             else { // 移除重复的分割线
-                                return array[index - 1].mode !== MenuItemMode.separator;
+                                return array[index - 1]?.mode !== MenuItemMode.separator;
                             }
                         default:
                             return true;
@@ -165,17 +162,17 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
                 });
 
             /* 生成菜单项 */
-            features.forEach(feature => {
+            features.forEach((feature) => {
                 switch (feature.mode) {
                     /* 文本输入框 */
                     case MenuItemMode.input: {
                         /* 派遣多个编辑任务: 如需编辑多个属性, 则需多个输入框 */
-                        feature.tasks.forEach(task => {
+                        feature.tasks?.forEach((task) => {
                             submenu.push({
                                 id: feature.id,
                                 element: globalThis.document.createElement("div"), // 避免生成其他内容
                                 disabled: !feature.enable,
-                                bind: element => handlers[task.type](
+                                bind: (element) => handlers[task.type](
                                     this,
                                     feature,
                                     context,
@@ -197,7 +194,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
                             accelerator: feature.accelerator,
                             disabled: !feature.enable,
                             click: async () => {
-                                for (const task of feature.tasks) {
+                                for (const task of (feature.tasks ?? [])) {
                                     await handlers[task.type](
                                         this,
                                         feature,
@@ -227,7 +224,7 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
                 submenu,
             });
         }
-    }
+    };
 
     /* 重置插件配置 */
     public async resetConfig(): Promise<void> {
@@ -246,8 +243,8 @@ export default class CustomBlockPlugin extends siyuan.Plugin {
     /* 更新根节点属性 */
     public updateRootAttr() {
         // const features = this.config.features.filter(feature => feature.enable && feature.style && feature.token); // 激活的功能
-        const features = this.config.features.filter(feature => !(feature.enable && feature.style && feature.token)); // 禁用的功能
-        const tokens = features.map(feature => feature.token); // 禁用的功能令牌列表
+        const features = this.config.features.filter((feature) => !(feature.enable && feature.style && feature.token)); // 禁用的功能
+        const tokens = features.map((feature) => feature.token); // 禁用的功能令牌列表
 
         /* 设置 HTML 根节点的属性 */
         globalThis.document.documentElement.setAttribute(
