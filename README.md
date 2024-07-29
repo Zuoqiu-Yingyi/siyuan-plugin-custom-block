db4a3bcbda2a05a969819e4716ecce238e513df0
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-07-30 00:00:04 +0800

    style(custom-block): 将 `eslint` 配置应用于 `custom-block` 模块 | Apply `eslint` configuration to `custom-block` module

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 <img alt="icon" src="./public/icon.png" style="width: 8em; height: 8em;">
 
 ---
+
 [![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/Zuoqiu-Yingyi/siyuan-plugin-custom-block?include_prereleases&style=flat-square)](https://github.com/Zuoqiu-Yingyi/siyuan-plugin-custom-block/releases/latest)
 [![GitHub Release Date](https://img.shields.io/github/release-date/Zuoqiu-Yingyi/siyuan-plugin-custom-block?style=flat-square)](https://github.com/Zuoqiu-Yingyi/siyuan-plugin-custom-block/releases/latest)
 [![GitHub License](https://img.shields.io/github/license/Zuoqiu-Yingyi/siyuan-plugin-custom-block?style=flat-square)](https://github.com/Zuoqiu-Yingyi/siyuan-plugin-custom-block/blob/main/LICENSE)
@@ -13,6 +14,7 @@
 [![GitHub all releases](https://img.shields.io/github/downloads/Zuoqiu-Yingyi/siyuan-plugin-custom-block/total?style=flat-square)](https://github.com/Zuoqiu-Yingyi/siyuan-plugin-custom-block/releases)
 
 ---
+
 </div>
 
 ## USER GUIDE
@@ -30,10 +32,10 @@
    ```
 3. Create a pull request from `dev` branch to `main` branch in sub-repository.
 4. Merge the pull request.
-5. Await for the CD workflow `release-please.yml` to complete, it will create a *release pull request* in sub-repository.
-6. Merge the *release pull request*, it will create a new *pre-release* with current [changelog](./CHANGELOG.md) and a new *tag* with [semantic version](https://semver.org/) in sub-repository.
+5. Await for the CD workflow `release-please.yml` to complete, it will create a _release pull request_ in sub-repository.
+6. Merge the _release pull request_, it will create a new _pre-release_ with current [changelog](./CHANGELOG.md) and a new _tag_ with [semantic version](https://semver.org/) in sub-repository.
 7. Await for the CD workflow `build.yml` to complete, it will update the distribution files to `publish` branch in sub-repository.
-8. Await for the CD workflow `release-distribution.yml` to complete, it will create a new *pre-release* with an asset named `package.zip` and a new *tag* with timestamp in sub-repository.
+8. Await for the CD workflow `release-distribution.yml` to complete, it will create a new _pre-release_ with an asset named `package.zip` and a new _tag_ with timestamp in sub-repository.
 
 ## CHANGELOG
 
--- src/types/task.d.ts
+++ src/types/task.d.ts
@@ -1,49 +1,79 @@
-/**
- * Copyright (C) 2023 Zuoqiu Yingyi
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2023 Zuoqiu Yingyi
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { TaskType } from "@/utils/enums";
+import type { TaskType } from "@/utils/enums";
 
 /* 块菜单任务 */
-export type Task = IEditTask | IToggleTask | iconFullscreen;
+export type Task =
+    | IDeleteTask
+    | IEditTask
+    | IFullscreenTask
+    | ISwitchTask
+    | IToggleTask
+    | IUpdateTask;
 
 /* 块菜单任务 */
 export interface ITask {
-    type: TaskType, // 任务类型
-    params: any, // 任务输入参数
+    type: TaskType; // 任务类型
+    params?: any; // 任务输入参数
+}
+
+/* 块菜单属性删除任务 */
+export interface IDeleteTask extends ITask {
+    type: TaskType.delete; // 任务类型
+    params: { // 任务输入参数
+        name: string; // 待删除的块属性名称
+    };
 }
 
 /* 块菜单属性编辑任务 */
 export interface IEditTask extends ITask {
-    type: TaskType, // 任务类型
+    type: TaskType.edit; // 任务类型
     params: { // 任务输入参数
-        name: string, // 待编辑的块属性名称
-    },
-}
-
-/* 块菜单属性 token 增删任务 */
-export interface IToggleTask extends ITask {
-    type: TaskType, // 任务类型
-    params: { // 任务输入参数
-        name: string, // 属性名
-        token: string, // 需要增删的 token
-    },
+        name: string; // 待编辑的块属性名称
+    };
 }
 
 /* 全屏任务 */
 export interface IFullscreenTask extends ITask {
-    type: TaskType, // 任务类型
+    type: TaskType.fullscreen; // 任务类型
+}
+
+/* 块菜单属性切换任务 */
+export interface ISwitchTask extends ITask {
+    type: TaskType.switch; // 任务类型
+    params: { // 任务输入参数
+        name: string; // 属性名
+        values: (null | string)[]; // 需要切换的属性值
+    };
+}
+
+/* 块菜单属性 token 增删任务 */
+export interface IToggleTask extends ITask {
+    type: TaskType.toggle; // 任务类型
+    params: { // 任务输入参数
+        name: string; // 属性名
+        token: string; // 需要增删的 token
+    };
+}
+
+/* 块菜单属性更新任务 */
+export interface IUpdateTask extends ITask {
+    type: TaskType.update; // 任务类型
+    params: { // 任务输入参数
+        name: string; // 待编辑的块属性名称
+        value: string; // 需要更新的块属性值
+    };
 }
