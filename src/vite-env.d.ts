9f9ffeb60210e206831ffbd6fbcb8c66a3c74f63
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-09-16 22:36:31 +0300

    style(monaco-editor): 将 `eslint` 配置应用于 `monaco-editor` 模块 | Apply `eslint` configuration to `monaco-editor` module

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+/// <reference types="@workspace/types/global" />
 
 declare module "*.svelte" {
     export { SvelteComponent as default } from "svelte";
