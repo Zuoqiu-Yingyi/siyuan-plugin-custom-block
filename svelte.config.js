c7990e19e003e6ddb48fbf00940e211008355b95
颖逸 <49649786+Zuoqiu-Yingyi@users.noreply.github.com>
2024-11-21 00:57:42 +0800

    build(svelte): 修复 Svelte 无法解析 `enum` 等 TypeScript 特性问题 | Fix the issue that Svelte cannot parse TypeScript features such as `enum`.

--- svelte.config.js
+++ svelte.config.js
@@ -18,5 +18,5 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
-    preprocess: vitePreprocess(),
+    preprocess: vitePreprocess({ script: true }),
 };
